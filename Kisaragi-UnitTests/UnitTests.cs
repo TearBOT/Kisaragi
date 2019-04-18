4315ab06d90a8b88ae90c659062e967c9d68d275
Xwilarg <xwilarg@yahoo.fr>
2019-04-18 18:05:17 +0200

    Add CI and unit tests project

---
+++ Kisaragi-UnitTests/UnitTests.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace Kisaragi_UnitTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public async Task Test()
+        { }
+    }
+}
