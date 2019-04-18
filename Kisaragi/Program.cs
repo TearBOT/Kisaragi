6654c69f07a1e79b683d8cd06a89e2ddadf1fa22
Xwilarg <xwilarg@yahoo.fr>
2019-04-18 17:54:56 +0200

    Add empty Discord bot

---
+++ Kisaragi/Program.cs
@@ -0,0 +1,56 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using DiscordUtils;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Kisaragi
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public readonly DiscordSocketClient client;
+        private readonly CommandService commands = new CommandService();
+
+        public DateTime StartTime { private set; get; }
+        public static Program P { private set; get; }
+
+        private Program()
+        {
+            P = this;
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Verbose,
+            });
+            client.Log += Utils.Log;
+            commands.Log += Utils.LogError;
+        }
+
+        private async Task MainAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+
+            await client.LoginAsync(TokenType.Bot, File.ReadAllText("Keys/token.txt"));
+            StartTime = DateTime.Now;
+            await client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage arg)
+        {
+            SocketUserMessage msg = arg as SocketUserMessage;
+            if (msg == null || arg.Author.IsBot) return;
+            int pos = 0;
+            if (msg.HasMentionPrefix(client.CurrentUser, ref pos) || msg.HasStringPrefix("n.", ref pos))
+            {
+                SocketCommandContext context = new SocketCommandContext(client, msg);
+                await commands.ExecuteAsync(context, pos, null);
+            }
+        }
+    }
+}
